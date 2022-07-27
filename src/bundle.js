cf5afd03128de9dca5592b31f6a1bd6b801e7adb
kyahia <yahia.ah.kerdache@gmail.com>
2022-07-27 19:35:37 +0100

    v:2.0 cdn + compiled styles tricolor-primary

--- src/bundle.js
+++ src/bundle.js
@@ -30,25 +30,49 @@ document.addEventListener("DOMContentLoaded", function () {
       })
    }
    // end if innerWidth
-}); 
+});
 
 // Scroll-top button 
 //Get the button:
 mybutton = document.getElementById("scroll-top-btn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+   } else {
+      mybutton.style.display = "none";
+   }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+   document.body.scrollTop = 0; // For Safari
+   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+document.querySelectorAll('.horizontal-rule button').forEach(button => {
+   button.addEventListener('click', e => {
+      const theme = e.target.className;
+      const link = document.createElement("link");
+      link.href = `./src/style-${theme}.css`
+      link.type = "text/css";
+      link.rel = "stylesheet";
+      link.media = "screen,print";
+
+      document.getElementsByTagName("head")[0].appendChild(link);
+
+      if (theme === "gold"){
+         document.getElementById('navbar').classList.remove('navbar-dark');
+         document.querySelector(':root').style.setProperty('--prime-color', 'gold');
+      } else if (theme === "red"){
+         document.querySelector(':root').style.setProperty('--prime-color', 'brown');
+         document.getElementById('navbar').classList.add('navbar-dark');
+      } else if (theme === "blue") {
+         document.querySelector(':root').style.setProperty('--prime-color', '#0d6efd');
+         document.getElementById('navbar').classList.add('navbar-dark');
+      }
+   })
+})
